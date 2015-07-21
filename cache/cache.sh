36169ed2b64b1b8a5fff6374bc87c5ef6b2f84b0
Daniel Farrell <dfarrell@redhat.com>
2015-07-21 14:44:39 -0400

    Add logic for caching ODL's RPM, other refactoring
    Signed-off-by: Daniel Farrell <dfarrell@redhat.com>

--- cache/cache.sh
+++ cache/cache.sh
@@ -6,26 +6,26 @@
 
 install_extras=true
 
-# NB: These will need to be updated for version bumps
-odl_version="0.3.0-Lithium"
-
 # Common names used in this script
-odl_tarball="distribution-karaf-$odl_version.tar.gz"
+odl_tarball="distribution-karaf-0.3.0-Lithium.tar.gz"
+odl_rpm="opendaylight-3.0.0-2.el7.centos.noarch.rpm"
 centos_iso="CentOS-7-x86_64-Minimal-1503-01.iso"
 centos_vagrant_box="chef-centos-7.0-virtualbox-1.0.0.box"
 odl_vagrant_box="opendaylight-2.3.0-centos-1503.box"
 odl_img_name="dfarrell07/odl:0.2.3"
-odl_container="dfarrell07-odl-0.2.3.tar"
+odl_img_tb="dfarrell07-odl-0.2.3.tar"
 
 # Common paths used in this script
 # TODO: Smarter cache paths
-odl_tb_url="https://nexus.opendaylight.org/content/groups/public/org/opendaylight/integration/distribution-karaf/$odl_version/$odl_tarball"
+odl_tb_url="https://nexus.opendaylight.org/content/groups/public/org/opendaylight/integration/distribution-karaf/0.3.0-Lithium/$odl_tarball"
 odl_tb_cache_path="$odl_tarball"
+odl_rpm_url="http://104.131.189.230/repository/$odl_rpm"
+odl_rpm_cache_path="$odl_rpm"
 centos_iso_cache_path="$centos_iso"
 centos_iso_url="http://mirrors.seas.harvard.edu/centos/7/isos/x86_64/$centos_iso"
 centos_vagrant_box_cache_path="$centos_vagrant_box"
 odl_vagrant_box_cache_path="$odl_vagrant_box"
-odl_container_cache_path="$odl_container"
+odl_img_tb_cache_path="$odl_img_tb"
 
 artifact_cached()
 {
@@ -61,14 +61,20 @@ dl_artifact()
 
   if ! artifact_cached $cache_path; then
     curl -o $cache_path $url
+    assert_artifact_cached $cache_path
   fi
-
-  assert_artifact_cached $cache_path
 }
 
 cache_odl_tb()
 {
+  # Download OpenDaylight's tarball release artifact if it's not cached locally
   dl_artifact $odl_tb_url $odl_tb_cache_path
+}
+
+cache_odl_rpm()
+{
+  # Download OpenDaylight's RPM if it's not cached locally
+  dl_artifact $odl_rpm_url $odl_rpm_cache_path
 }
 
 cache_centos_iso()
@@ -96,7 +102,7 @@ cache_centos_vagrant_box()
 
 cache_odl_vagrant_box()
 {
-  # TODO: Download OpenDaylight's Vagrant base box if it's not cached locally
+  # Download OpenDaylight's Vagrant base box if it's not cached locally
   if ! artifact_cached $odl_vagrant_box_cache_path; then
     # Download a CentOS 7 Vagant base box if it's not cached by Vagrant
     vagrant box add --provider virtualbox dfarrell07/opendaylight
@@ -111,21 +117,22 @@ cache_odl_vagrant_box()
   fi
 }
 
-cache_odl_container()
+cache_odl_img_tb()
 {
   # Download ODL's container (via Docker for now) if it's not cached locally
-  if ! artifact_cached $odl_container_cache_path; then
+  if ! artifact_cached $odl_img_tb_cache_path; then
     # Download ODL's image from DockerHub if it's not cached locally
     docker pull $odl_img_name
 
     # Build a tarball from the local version pulled above. Docker doesn't
     #   have a way to download image tarballs directly, so two steps.
-    docker save --output="$odl_container_cache_path" $odl_img_name
+    docker save --output="$odl_img_tb_cache_path" $odl_img_name
 
     # Confirm ODL's container was output to the expected location
-    assert_artifact_cached $odl_container_cache_path
+    assert_artifact_cached $odl_img_tb_cache_path
   fi
 }
+
 
 cache_odl_tb
 cache_centos_iso
@@ -133,5 +140,6 @@ cache_centos_vagrant_box
 
 if [ "$install_extras" == true ]; then
   cache_odl_vagrant_box
-  cache_odl_container
+  cache_odl_img_tb
+  cache_odl_rpm
 fi
