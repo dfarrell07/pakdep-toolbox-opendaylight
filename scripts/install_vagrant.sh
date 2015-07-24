f9dc0947fd0bd6850e6bf75751d98cc0cff04ef4
Daniel Farrell <dfarrell@redhat.com>
2015-07-24 01:41:03 -0400

    Remove unused provisioning scripts
    Signed-off-by: Daniel Farrell <dfarrell@redhat.com>

--- scripts/install_vagrant.sh
+++
@@ -1,6 +0,0 @@
-# Install Vagrant
-yum localinstall -y https://dl.bintray.com/mitchellh/vagrant/vagrant_1.7.2_x86_64.rpm
-
-# Install a plugin that will update the VBox Guest Additions version
-#   of Vagrant boxes that we start *from/inside* the pakdep Vagrant box.
-vagrant plugin install vagrant-vbguest
